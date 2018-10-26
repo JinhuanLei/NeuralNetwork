49567371a9590ad402989be5e22febe80f555e4b
JinhuanLei <lei.jinhuan@uwlax.edu>
2018-10-25 22:42:02 -0500

    Add the loadData module and RegExpression part

--- NN.py
+++ NN.py
@@ -1,3 +1,10 @@
+import networkx as nx
+import os
+import re
+
+CURRENT_PATH = os.path.dirname(__file__)
+
+
 def getInputs():
     options = str(input("Enter L to load trained network, T to train a new one, Q to quit: "))
     if options == "L" or options == "l":
@@ -11,16 +18,52 @@ def getInputs():
             else:
                 print("Load Training Set.")
                 print("Load Testing Set.")
+                loadData(resolution)
                 return
     else:
         print("Goodbye.")
         return
 
 
-getInputs()
+def loadData(resolution):
+    if resolution == "5":
+        resolution = "0" + resolution
+    trainSetFileName = "trainSet_" + resolution + ".dat"
+    testSetFileName = "testSet_" + resolution + ".dat"
+    trainSetPath = CURRENT_PATH + "/" + "trainSet_data/" + trainSetFileName
+    testSetFilePath = CURRENT_PATH + "/" + "testSet_data/" + testSetFileName
+    if not (os.path.exists(trainSetPath) and os.path.exists(testSetFilePath)):
+        print("Can not find the file, NN.py should in the same folder with the trainSet_data and testSet_data")
+    trainSet = []
+    trainLable = []
+    testSet = []
+    testLabel = []
+    with open(trainSetPath, "r") as f:
+        for line in f.readlines():
+            line = line.strip()
+            if line[0] == "#":
+                continue
+            regExpList = re.findall(r'[(](.*?)[)]', line)
+            trainSet.append(list(map(float, regExpList[0].split(" "))))
+            trainLable.append(list(map(float, regExpList[1].split(" "))))
+    with open(testSetFilePath, "r") as f:
+        for line in f.readlines():
+            line = line.strip()
+            if line[0] == "#":
+                continue
+            regExpList = re.findall(r'[(](.*?)[)]', line)
+            testSet.append(list(map(float, regExpList[0].split(" "))))
+            testLabel.append(list(map(float, regExpList[1].split(" "))))
+    print("length", len(testSet), "  ", len(testLabel))
+    print(testSet[0])
+    print(testLabel[0])
 
 
 class Neuron(object):
-    def __init__(self, tag, weight):
+    def __init__(self, tag, linkedNeurons):
         self.tag = tag
-        self.weight = weight
+        self.linkedNeurons = linkedNeurons
+
+
+if __name__ == "__main__":
+    getInputs()
