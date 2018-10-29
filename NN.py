47d2ef716de388469302dd38a2ee8a8da39de283
JinhuanLei <lei.jinhuan@uwlax.edu>
2018-10-29 00:46:49 -0500

    need prove some assumptions (need talk with prof.)

--- NN.py
+++ NN.py
@@ -62,7 +62,111 @@ def loadData(resolution):
             regExpList = re.findall(r'[(](.*?)[)]', line)
             testSet.append(list(map(float, regExpList[0].split(" "))))
             testLabel.append(list(map(float, regExpList[1].split(" "))))
-    initNeurons()
+    initNeurons(trainSetFileName, testSetFileName)
+
+
+def initNeurons(trainSetFileName, testSetFileName):
+    print("Initializing network...")
+    hiddenLayers = initHiddenLayer()
+    inputLayer = initInputLayer(len(trainSet[0]))
+    outputLayer = initOutputLayer(len(trainLable[0]))
+    network = []
+    network.append(inputLayer)
+    network = network + hiddenLayers
+    network.append(outputLayer)
+    network = initWeight(network)
+    trainedNetwork = backPropagationLearning(network, trainSetFileName)
+    validateNetwork(trainedNetwork, testSetFileName)
+
+
+def validateNetwork(network, testSetFileName):
+    print("Testing on " + testSetFileName + "...")
+    correct = 0
+    for (image, lable) in zip(testSet, testLabel):
+        countNeuron = 0
+        for neu in network[0]:
+            neu.setActivationFunctionOutput(image[countNeuron])
+            countNeuron += 1
+        for i in range(1, len(network)):
+            for neo in network[i]:
+                input = neo.weight
+                a = getActivationOutputVector(network[i - 1])
+                # print(len(a), "______________", neo.bias)
+                newAVal = np.dot(a, input) + neo.bias  # can not reverse order (65, ) dot (65, 1)
+                neo.setActivationFunctionOutput(sigmoid(newAVal))
+        countNeuron = 0
+        flag = 1
+        for neu in network[len(network) - 1]:
+            y = lable[countNeuron]
+            judge = 0
+            if(neu.aVal>0):
+                judge = 1
+            else:
+                judge = 0
+            if y != judge:
+                flag = 0
+                break
+            countNeuron += 1
+        if flag == 1:
+            correct += 1
+    print("Accuracy achieved: ", correct / len(testSet))
+
+
+def backPropagationLearning(network, trainSetFileName):
+    # Propagate the inputs forward to compute the outputs
+    print("Training on " + trainSetFileName + "...")
+    for num in range(50):
+        for (timage, tlable) in zip(trainSet, trainLable):
+            # input layer
+            countNeuron = 0
+            for neu in network[0]:
+                neu.setActivationFunctionOutput(timage[countNeuron])
+                countNeuron += 1
+            for i in range(1, len(network)):
+                for neo in network[i]:
+                    input = neo.weight
+                    a = getActivationOutputVector(network[i - 1])
+                    # print(len(a), "______________", neo.bias)
+                    newAVal = np.dot(a, input) + neo.bias  # can not reverse order (65, ) dot (65, 1)
+                    neo.setActivationFunctionOutput(sigmoid(newAVal))
+            # Propagate deltas backward from output layer to input layer    Some problems
+            # output layer
+            countNeuron = 0
+            deltaVal = 0
+            for neu in network[len(network) - 1]:
+                y = tlable[countNeuron]
+                derivativeVal = getDerivativeVal(neu.aVal)
+                # if y - neu.aVal < 0.01:
+                #     print("Opps")
+                #     return network
+                deltaVal = derivativeVal * (y - neu.aVal)
+                countNeuron += 1
+                # Update every weight in network using deltas
+            for i in range(len(network) - 2, 0, -1):
+                count = 0
+                for neu in network[i]:
+                    a = getActivationOutputVector(network[i - 1])
+                    oldWeight = getWeightVectorFromLastLayer(network, count, i + 1)
+                    derivativeVal = getDerivativeVal(neu.aVal)
+                    newDelta = getNewDeltaVal(deltaVal, oldWeight, derivativeVal)
+                    for j in range(len(neu.weight)):
+                        neu.weight[j] = neu.weight[j] + newDelta * a[j]
+                    neu.bias = neu.bias + newDelta  # dummy value always 1 ??
+                    count += 1
+    return network
+
+
+def getActivationOutputVector(layer):
+    list = []
+    for neu in layer:
+        list.append(neu.aVal)
+    list = np.array(list)
+    return list
+
+
+def sigmoid(z):
+    s = 1 / (1 + math.exp(-z))
+    return s
 
 
 def initWeightHelper(dim):
@@ -81,63 +185,23 @@ def initWeight(network):
     return network
 
 
-def initNeurons():
-    hiddenLayers = initHiddenLayer()
-    inputLayer = initInputLayer(len(trainSet[0]))
-    outputLayer = initOutputLayer(len(trainLable[0]))
-    network = []
-    network.append(inputLayer)
-    network = network + hiddenLayers
-    network.append(outputLayer)
-    network = initWeight(network)
-    trainedNetwork = backPropagationLearning(network)
+def getNewDeltaVal(deltaVal, oldWeight, derivativeVal):
+    count = 0.0
+    for w in oldWeight:
+        count = count + deltaVal * w
+    return count * derivativeVal
 
 
-def getActivationOutputVector(layer):
-    list = []
+def getDerivativeVal(aVal):
+    return aVal * (1 - aVal)
+
+
+def getWeightVectorFromLastLayer(network, neuronIndex, layerIndex):
+    vector = []
+    layer = network[layerIndex]
     for neu in layer:
-        list.append(neu.aVal)
-    list = np.array(list)
-    return list
-
-
-def sigmoid(z):
-    s = 1 / (1 + math.exp(-z))
-    return s
-
-
-def backPropagationLearning(network):  # miss add bias!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    # Propagate the inputs forward to compute the outputs
-    for (timage, tlable) in zip(trainSet, trainLable):
-        # input layer
-        countNeuron = 0
-        for neu in network[0]:
-            neu.setActivationFunctionOutput(timage[countNeuron])
-            countNeuron += 1
-        for i in range(1, len(network)):
-            for neo in network[i]:
-                input = neo.weight
-                a = getActivationOutputVector(network[i - 1])
-                # print(len(a), "______________", neo.bias)
-                newAVal = np.dot(a, input) + neo.bias  # can not reverse order (65, ) dot (65, 1)
-                neo.setActivationFunctionOutput(sigmoid(newAVal))
-        # Propagate deltas backward from output layer to input layer    Some problems
-        # output layer
-        countNeuron = 0
-        deltaVal = 0
-        for neu in network[len(network) - 1]:
-            y = tlable[countNeuron]
-            derivativeVal = neu.aVal * (1 - neu.aVal)
-            deltaVal = derivativeVal * (y - neu.aVal)
-            countNeuron += 1
-             # Update every weight in network using deltas
-            for i in range(len(network), 0, -1):
-                for neu in network[i]:
-                    a = getActivationOutputVector(network[i - 1])
-                    for j in range(len(neu.weight)):
-                        neu.weight[j] = neu.weight[j] + deltaVal * a[j]
-                    neu.bias = neu.bias + deltaVal  # dummy value always 1 ??
-    return network
+        vector.append(neu.weight[neuronIndex])
+    return vector
 
 
 def initHiddenLayer():
